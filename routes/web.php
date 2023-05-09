f0b65dfa83e4bfb659d7c8def43a9b39e7ad98e2
Marco Rinaldi <marcorinaldi98@libero.it>
2023-05-09 13:56:28 +0200

    visualizzo "hello world"

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $messaggio = "Hello World!";
+
+    $data = [
+        'messaggio' => $messaggio
+    ];
+
+    return view('homepage', $data);
 });
